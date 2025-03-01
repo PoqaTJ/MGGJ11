52a2b423547d93269a95a1f19d0526837dae97e1
PoqaTJ <Tbjohnson42@gmail.com>
2025-03-01 15:45:18 -0800

    Break barriers powerup loc and collectable

--- Assets/Scripts/Game/Collectable.cs
+++ Assets/Scripts/Game/Collectable.cs
@@ -6,10 +6,13 @@ namespace Game
 {
     public class Collectable: MonoBehaviour
     {
+        private bool _pickedUp = false;
+        
         private void OnTriggerEnter2D(Collider2D col)
         {
-            if (col.tag == "Player")
+            if (!_pickedUp && col.CompareTag("Player"))
             {
+                _pickedUp = true;
                 OnContact();
             }
         }
---
+++ Assets/Scripts/Game/PowerupBreakHazards.cs
@@ -0,0 +1,29 @@
+﻿using Menus;
+using Menus.MenuTypes;
+using Services;
+
+namespace Game
+{
+    public class PowerupBreakHazards: Collectable
+    {
+        void Start()
+        {
+            if (ServiceLocator.Instance.SaveManager.UnlockedBreakHazard)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
+        protected override void OnContact()
+        {
+            var context = new PopupMenuOneButton.PopupMenuOneButtonContext();
+            context.titleLocString = "dialog-powerup-break-hazards-title";
+            context.bodyLocString = "dialog-powerup-break-hazards-body";
+            context.buttonLocString = "dialog-powerup-break-hazards-button";
+            ServiceLocator.Instance.MenuManager.Show(MenuType.PopupOneButton, context);
+            
+            ServiceLocator.Instance.SaveManager.UnlockedBreakHazard = true;
+            base.OnContact();
+        }
+    }
+}
