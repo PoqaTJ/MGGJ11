c209085da48ef060c72cee7b0066c1c77ba5fff3
PoqaTJ <Tbjohnson42@gmail.com>
2025-03-01 10:38:05 -0800

    Automate replacing level elements with prefabs.

--- Assets/Scripts/Game/Editor/LevelCleanup.cs
+++ Assets/Scripts/Game/Editor/LevelCleanup.cs
@@ -6,20 +6,49 @@ namespace Game.Editor
 {
     public class LevelCleanup
     {
-        [MenuItem("Level/CountPlatformTypes")]
-        private static void ClassifyLedges()
+        [MenuItem("Level/PrefabifySpawners")]
+        private static void PrefabifySpawners()
         {
             GameObject root = GameObject.Find("Level");
             if (root == null)
             {
                 Debug.LogError("Couldn't find level root.");
+                return;
+            }
+
+            int count = 0;
+            GameObject prefab = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Spawner.prefab");
+            for (int i = 0; i < root.transform.childCount; i++)
+            {
+                GameObject c = root.transform.GetChild(i).gameObject;
+                if (c.name.StartsWith("Spawner"))
+                {
+                    count++;
+                    var go = (GameObject)PrefabUtility.InstantiatePrefab(prefab);
+                    go.transform.SetParent(root.transform);
+                    go.transform.position = c.transform.position;
+                    GameObject.DestroyImmediate(c);
+                }
+            }
+            
+            Debug.Log($"Prefabified {count} instances.");
+        }
+
+        [MenuItem("Level/PrefabifyLedges")]
+        private static void PrefabifyLedges()
+        {
+            GameObject root = GameObject.Find("Level");
+            if (root == null)
+            {
+                Debug.LogError("Couldn't find level root.");
+                return;
             }
 
             var platforms = new List<GameObject>();
             for (int i = 0; i < root.transform.childCount; i++)
             {
                 GameObject c = root.transform.GetChild(i).gameObject;
-                if (c.name.StartsWith("Ledge"))
+                if (c.name.StartsWith("Ledge") && !c.name.Contains("Clone"))
                 {
                     platforms.Add(c);
                 }
@@ -43,6 +72,29 @@ namespace Game.Editor
             foreach (var k in platformCounts.Keys)
             {
                 Debug.Log($"Type {k}: {platformCounts[k]}");
+            }
+            
+            
+            // try replacing prefabs
+            var prefabsDict = new Dictionary<string, GameObject>();
+            prefabsDict["1,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-1.prefab");
+            prefabsDict["1.5,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-1p5.prefab");
+            prefabsDict["2,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-2.prefab");
+            prefabsDict["3,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-3.prefab");
+            prefabsDict["4,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-4.prefab");
+            prefabsDict["5,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-5.prefab");
+            prefabsDict["10,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-10.prefab");
+            prefabsDict["27,0.25"] = AssetDatabase.LoadAssetAtPath<GameObject>("Assets/Prefabs/Level/Platforms/Ledge-27.prefab");
+            foreach (var p in platforms)
+            {
+                string type = $"{p.transform.localScale.x},{p.transform.localScale.y}";
+                if (prefabsDict.ContainsKey(type))
+                {
+                    var go = (GameObject)PrefabUtility.InstantiatePrefab(prefabsDict[type]);
+                    go.transform.SetParent(root.transform);
+                    go.transform.position = p.transform.position;
+                    GameObject.DestroyImmediate(p);
+                }
             }
         }
     }
