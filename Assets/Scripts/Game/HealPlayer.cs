61e159f5bb32612d85bc6e476e08374c8a8538f3
PoqaTJ <Tbjohnson42@gmail.com>
2025-03-01 11:49:45 -0800

    Spawners heal the player to full.

---
+++ Assets/Scripts/Game/HealPlayer.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Player;
+using Unity.VisualScripting;
+using UnityEngine;
+
+namespace Game
+{
+    public class HealPlayer: MonoBehaviour
+    {
+        [SerializeField] private int _health = 1;
+        private void OnTriggerEnter2D(Collider2D col)
+        {
+            if (col.CompareTag("Player"))
+            {
+                var playerController = col.GetComponent<PlayerController>();
+                if (playerController)
+                {
+                    playerController.Heal(_health);                    
+                }
+            }
+        }
+    }
+}
